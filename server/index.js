a4c647b88270c0f59cf2647bb33a53db00c8658b
rushna <rushnaulaziz@gmail.com>
2022-02-21 12:26:32 +0500

    Adds load-data and save-data endpoints in server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+app.get("/api/load-data", (req, res) => {
+  
+    fs.readFile('./server/data.json', (err, data) => {
+        if (err) throw err;
+        res.json({data: JSON.parse(data).data});
+    });
+   
+});
+
+app.post("/api/store-data", (req, res) => {
+  
+    fs.readFile('./server/data.json', (err, data) => {
+        if (err) throw err;
+        res.json({data: JSON.parse(data).data});
+    });
+   
 });
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
