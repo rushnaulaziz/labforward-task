26bb8a8dbe15ffc7020d3c79ab9dcf696d29e1f0
rushna <rushnaulaziz@gmail.com>
2022-02-21 15:26:48 +0500

    implements save-data endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const fs = require('fs');
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/api/load-data", (req, res) => {
   
@@ -15,13 +17,19 @@ app.get("/api/load-data", (req, res) => {
    
 });
 
-app.post("/api/store-data", (req, res) => {
+app.post("/api/save-data", (req, res) => {
   
-    fs.readFile('./server/data.json', (err, data) => {
-        if (err) throw err;
-        res.json({data: JSON.parse(data).data});
-    });
-   
+ console.log(req.body)
+ fs.writeFile("./server/data.json", JSON.stringify(req.body), 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        res.json({message: err})
+    }
+ 
+    console.log("JSON file has been saved.");
+    res.json({message: "Data has been saved"})
+});
+
 });
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
